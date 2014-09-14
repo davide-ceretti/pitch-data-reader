5dcf9239674684d8bcc627081996e35f1b507329
Davide Ceretti <dav.ceretti@gmail.com>
2014-09-14 19:13:07 +0100

    Refactor tests

--- main.py
+++ main.py
@@ -1,36 +1,3 @@
-"""
-ADD ORDER
-Field Name | Offset | Length | Data Type | Description
-------------------------------------------------------
-Timestamp  | 0      | 8      | Numeric   | Timestamp
-Msg Type   | 8      | 1      | "A"       | Add Order
-Order ID   | 9      | 12     | Base 36   | Order Identifier
-Site Indic | 21     | 1      | Alpha     | [B]uy or [S]Ell
-Shares     | 22     | 6      | Numeric   | N. of Shares added
-Stock Symb | 28     | 6      | Alpha     | Stock Symbol
-Price      | 34     | 10     | Numeric   | Limit order Price
-Display    | 44     | 1      | Alpha     | "Y" = Displayed in SIP
-
-Example:
-[28800011][A][AK27GA0000DT][S][000100][SH    ][0000619200][Y]
-"""
-
-
-"""
-EXECUTE ORDER
-Field Name | Offset | Length | Data Type | Description
-------------------------------------------------------
-Timestamp  | 0      | 8      | Numeric   | Timestamp
-Msg Type   | 8      | 1      | "E"       | Execute Order
-Order ID   | 9      | 12     | Base 36   | Order Identifier
-Exc Shares | 21     | 6      | Numeric   | Number of shares executed
-Exc ID     | 27     | 12     | Base 36   | Execution Identifier
-
-Example:
-[28800318][E][AK27GA0000DT][000050][00001AQ00001]
-"""
-
-
 def pitch_parser(iterable):
     """
     Takes an iterable containing PITCH messages and returns a dictionary
--- tests.py
+++ tests.py
@@ -1,6 +1,74 @@
 import unittest
+from collections import OrderedDict
 
 from main import pitch_parser
+
+
+def create_add_order(**kwargs):
+    defaults = [
+        ('timestamp', "28800011"),
+        ('msg_type', "A"),
+        ('order_id', "AK27GA0000DT"),
+        ('site_indicator', "S"),
+        ('shares', "000100"),
+        ('stock_symbol', "SH    "),
+        ('price', "0000619200"),
+        ('display', "Y"),
+    ]
+    add_order_dict = OrderedDict()
+    for key, value in defaults:
+        add_order_dict[key] = value
+    add_order_dict.update(**kwargs)
+    return "".join(add_order_dict.itervalues())
+
+
+def create_execute_order(**kwargs):
+    defaults = [
+        ('timestamp', "28800011"),
+        ('msg_type', "E"),
+        ('order_id', "AK27GA0000DT"),
+        ('exec_shares', "000040"),
+        ('exec_id', "00001AQ00001"),
+    ]
+    add_order_dict = OrderedDict()
+    for key, value in defaults:
+        add_order_dict[key] = value
+    add_order_dict.update(**kwargs)
+    return "".join(add_order_dict.itervalues())
+
+
+class TestUtils(unittest.TestCase):
+    def test_create_add_order_default(self):
+        order = create_add_order()
+
+        self.assertEqual(
+            order,
+            "28800011AAK27GA0000DTS000100SH    0000619200Y"
+        )
+
+    def test_create_add_order_override_shares(self):
+        order = create_add_order(shares="999999")
+
+        self.assertEqual(
+            order,
+            "28800011AAK27GA0000DTS999999SH    0000619200Y"
+        )
+
+    def test_create_execute_order_default(self):
+        order = create_execute_order()
+
+        self.assertEqual(
+            order,
+            "28800011EAK27GA0000DT00004000001AQ00001"
+        )
+
+    def test_create_execute_order_override_order_id(self):
+        order = create_execute_order(order_id="XXXXXXXXXXXX")
+
+        self.assertEqual(
+            order,
+            "28800011EXXXXXXXXXXXX00004000001AQ00001"
+        )
 
 
 class TestPitchParser(unittest.TestCase):
@@ -12,7 +80,7 @@ class TestPitchParser(unittest.TestCase):
         self.assertDictEqual(result, {})
 
     def test_add_order(self):
-        data = ["28800011AAK27GA0000DTS000100SH    0000619200Y"]
+        data = [create_add_order()]
 
         result = pitch_parser(data)
 
@@ -20,13 +88,15 @@ class TestPitchParser(unittest.TestCase):
 
     def test_add_order_and_execute_order(self):
         data = [
-            "28800011AAK27GA0000DTS000100SH    0000619200Y",
-            "28800318EAK27GA0000DT00004000001AQ00001"
+            create_add_order(
+                stock_symbol='DAV   ', order_id='QQQWWWEEERRR', shares='000200'
+            ),
+            create_execute_order(order_id='QQQWWWEEERRR', exec_shares='000110')
         ]
 
         result = pitch_parser(data)
 
-        expected = {"SH": 40}
+        expected = {"DAV": 110}
         self.assertDictEqual(result, expected)
 
 if __name__ == "__main__":
